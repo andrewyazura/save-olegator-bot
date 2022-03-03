7782c4712bd956c7247d3badd546c2eed65ae32d
Andrew Yatsura <andrewyazura203@gmail.com>
2022-03-03 18:01:19 +0200

    remove ascii because library is outdated and causes conflicts on VPS

--- src/handlers/videos.py
+++ src/handlers/videos.py
@@ -1,13 +1,10 @@
 import os
-import time
 from uuid import uuid4
 
 import cv2
 import ffmpeg
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.error import BadRequest
 from telegram.ext import CommandHandler, ConversationHandler, Filters, MessageHandler
-from video_to_ascii.video_engine import VideoEngine
 
 from src import current_bot
 from src.helpers import rescale_frame
@@ -18,7 +15,7 @@ def get_strategy(update, context):
     user.send_message(
         "Choose strategy:",
         reply_markup=ReplyKeyboardMarkup(
-            [["ASCII"], ["Downscale"], ["H265 codec with sound"]],
+            [["Downscale"], ["H265 codec with sound"]],
             resize_keyboard=True,
             one_time_keyboard=True,
         ),
@@ -28,72 +25,6 @@ def get_strategy(update, context):
     context.user_data["file_id"] = file_id
 
     return 1
-
-
-@current_bot.log_update
-def ascii_video(update, context):
-    user = update.effective_user
-    user.send_message("Downloading...")
-
-    filename = uuid4().hex
-    video_file = f"media/{filename}.mp4"
-    bash_file = f"media/{filename}.sh"
-
-    file_id = context.user_data["file_id"]
-    file = current_bot.bot.get_file(file_id)
-    file.download(video_file)
-
-    user.send_message("Converting...")
-
-    engine = VideoEngine()
-    engine.load_video_from_file(video_file)
-    engine.set_strategy("just-ascii")
-    engine.render_strategy.render(
-        cap=engine.read_buffer, output=bash_file, with_audio=False
-    )
-
-    with open(bash_file, "r") as f:
-        f.readline()
-        f.readline()
-
-        frames = [
-            frame.strip().lstrip("\necho -en '\x1b[0;0H' \nsleep 0.033 \necho -en '")
-            for frame in f.read().split("\n'")
-        ]
-
-    message = current_bot.bot.send_message(
-        text=f"<pre>{frames[0]}</pre>", chat_id=user.id
-    )
-
-    fps = engine.read_buffer.get(cv2.CAP_PROP_FPS) or 30
-    delay = 1 / fps
-    n = 0
-    skip = 10
-
-    for frame in frames[1:-1]:
-        n += 1
-
-        if n % skip != 0:
-            continue
-        else:
-            n = n % skip
-
-        try:
-            current_bot.bot.edit_message_text(
-                text=f"<pre>{frame}</pre>",
-                chat_id=message.chat.id,
-                message_id=message.message_id,
-            )
-        except BadRequest:
-            user.send_message("bot is banned, waiting 10 second timeout")
-            time.sleep(10)
-        else:
-            time.sleep(delay * skip)
-
-    os.remove(video_file)
-    os.remove(bash_file)
-
-    return ConversationHandler.END
 
 
 @current_bot.log_update
@@ -233,7 +164,6 @@ current_bot.dispatcher.add_handler(
         entry_points=[MessageHandler(Filters.video, get_strategy)],
         states={
             1: [
-                MessageHandler(Filters.regex(r"^ASCII$"), ascii_video),
                 MessageHandler(Filters.regex(r"^Downscale$"), ask_downscale_percent),
                 MessageHandler(Filters.regex(r"^H265 codec with sound$"), ask_h265_crf),
             ],
